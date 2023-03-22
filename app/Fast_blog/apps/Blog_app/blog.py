9a483ed20a1f190a3942077e1eff3f39208038cf
Exp1oit Yao <eitsxiaozhai@gmail.com>
2023-03-22 16:22:30 +0800

    添加博客API

--- app/Fast_blog/apps/Blog_app/blog.py
+++ app/Fast_blog/apps/Blog_app/blog.py
@@ -1,15 +1,18 @@
 # ----- coding: utf-8 ------
 # author: YAO XU time:
-
+import dataclasses
+import json
 from typing import Optional
 from fastapi import FastAPI, Request
+from dataclasses import dataclass
 
+from flask import jsonify
 from sqlalchemy.orm import sessionmaker
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from fastapi import APIRouter
 from uvicorn.middleware.debug import HTMLResponse
-from sqlalchemy import select
+from sqlalchemy import select, text
 from app.Fast_blog.database.database import engine, db_session
 from app.Fast_blog.model import models
 from app.Fast_blog.schemas import schemas
@@ -25,16 +28,23 @@ BlogApp.mount("/static", StaticFiles(directory="./Fast_blog/static"), name="stat
 
 @BlogApp.get('/')
 async def index(request:Request):
-    return templates.TemplateResponse(name="/index_page/blog_html/index.html",context={"request":request})
+    async with db_session() as session:
+        try:
+            return templates.TemplateResponse(name="/index_page/blog_html/index.html", context={"request": request})
+        except Exception as e:
+            print("我们遇到了下面的问题")
+            print(e)
+        return 0
+
 
 
 @BlogApp.get('/about')
-async def index(request:Request):
+async def BlogAbout(request:Request):
     return templates.TemplateResponse(name="/index_page/blog_html/about.html",context={"request":request})
 
 
 @BlogApp.get('/contact')
-async def index(request:Request):
+async def BlogContact(request:Request):
     return templates.TemplateResponse(name="/index_page/blog_html/contact.html",context={"request":request})
 
 
@@ -49,6 +59,24 @@ async def BlogAdd(Addtitle:str,Addcontent:str,Addauthor:str,):
             session.add(x)
             await session.commit()
             print('文章已经添加到对应数据库')
+        except Exception as e:
+            print("我们遇到了下面的问题")
+            print(e)
+        return 0
+
+@BlogApp.get("/BlogIndex")
+##博客首页API
+async def BlogIndex():
+    async with db_session() as session:
+        try:
+            stmt = select(models.Blog).order_by(models.Blog.BlogId)
+            result = await session.execute(text("select * from blogtable LIMIT 3;"))
+            results = result.fetchall()
+            results = [tuple(row) for row in results]
+            print(f"{type(results)} of type {type(results[0])}")
+            # <class 'list'> of type <class 'tuple'>
+            json_string = json.dumps(results,ensure_ascii=False)
+            return ({"data":json_string})
         except Exception as e:
             print("我们遇到了下面的问题")
             print(e)
