630fd11e16263c9b4479b49f1ff6df8c49d53315
Exp1oit Yao <eitsxiaozhai@gmail.com>
2023-03-25 20:42:25 +0800

    添加电力爬取判断

--- app/Fast_blog/model/models.py
+++ app/Fast_blog/model/models.py
@@ -47,7 +47,7 @@ class PowerMeters(Base):
     __tablename__ = "powertable"
     __table_args__ = {'extend_existing': True}
     PowerId = Column(Integer, primary_key=True, index=True)
-    DataNum = Column(DateTime,default= datetime.datetime.now().strftime("%Y-%m-%d %H:%M"))
+    DataNum = Column(DateTime,default= datetime.datetime.now().strftime("%Y-%m-%d"))
     electricityNum = Column(String(255))
     PowerConsumption = Column(String(255))
     AveragePower = Column(Integer)
