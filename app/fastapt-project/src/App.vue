62f75c7bb0fb7e7ffb2193ead165cbaeb475c880
Exp1oit Yao <eitsxiaozhai@gmail.com>
2023-03-31 22:55:14 +0800

    创建Vue前后端分离

---
+++ app/fastapt-project/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+    <router-link to="/home">首页</router-link>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'App'
+  }
+</script>
+
+<style>
+
+</style>
