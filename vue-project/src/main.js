ad5cfa7d47d50c52b6a9eef8583169488c59cb2f
Exp1oit Yao <eitsxiaozhai@gmail.com>
2023-07-04 13:47:47 +0800

    完成后台任务自动定时执行

--- vue-project/src/main.js
+++
@@ -1,8 +0,0 @@
-import App from './App.vue'
-import { createApp } from 'vue'
-import Layui from '@layui/layui-vue'
-import '@layui/layui-vue/lib/index.css'
-import router from "./route" //匹配自己项目所对应的路径
-
-createApp(App).use(router).mount("#app") //使用配置的路由
-createApp(App).use(Layui).mount('#app')
