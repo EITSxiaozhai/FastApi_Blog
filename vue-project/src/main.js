93f087d53340e4cf98c3f0042ed2c601ffbf9c69
Exp1oit Yao <eitsxiaozhai@gmail.com>
2023-07-14 13:18:27 +0800

    修改页面

--- vue-project/src/main.js
+++ vue-project/src/main.js
@@ -2,11 +2,14 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import Particles from "vue3-particles";
-
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
 
 const app = createApp(App)
 
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component)
+}
 app.use(router)
 app.use(Particles)
 
