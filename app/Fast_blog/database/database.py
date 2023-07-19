bb5e9b166a5f8e9f12b2cb53c81c553ff0f15e46
Exp1oit Yao <eitsxiaozhai@gmail.com>
2023-07-19 17:04:56 +0800

    修改不同版本的数据连接

--- app/Fast_blog/database/database.py
+++ app/Fast_blog/database/database.py
@@ -1,18 +1,31 @@
+import os
 from urllib import  parse
 import  sys
 from asyncio import current_task
 
-
+from dotenv import load_dotenv
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_scoped_session
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-
 sys.path.append('../')
 
-password = parse.quote("@XU15964352xu")
-##使用utf8进行中文输入
-Sql_URL = "mysql+asyncmy://django:"+ password + "@mysql.exploit-db.xyz:63775/fastapi_blog" + "?charset=utf8mb4"
+load_dotenv()
+
+# 从环境变量中获取值
+db_username = os.getenv("DB_USERNAME")
+db_password = os.getenv("DB_PASSWORD")
+db_hostname = os.getenv("DB_HOSTNAME")
+db_port = os.getenv("DB_PORT")
+db_name = os.getenv("DB_NAME")
+
+db_password = parse.quote(db_password)
+
+Sql_URL = f"mysql+asyncmy://{db_username}:{db_password}@{db_hostname}:{db_port}/{db_name}?charset=utf8mb4"
+
+# password = parse.quote("@XU15964352xu")
+# ##使用utf8进行中文输入
+# Sql_URL = "mysql+asyncmy://django:"+ password + "@mysql.exploit-db.xyz:63775/fastapi_blog" + "?charset=utf8mb4"
 ##创建异步连接
 engine = create_async_engine(Sql_URL)
 
