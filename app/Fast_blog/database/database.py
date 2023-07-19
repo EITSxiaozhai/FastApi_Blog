79267bf682c1701e9d9c89e8b3031497865a7ec2
Exp1oit Yao <eitsxiaozhai@gmail.com>
2023-07-19 17:23:59 +0800

    修改不同版本的数据连接

--- app/Fast_blog/database/database.py
+++ app/Fast_blog/database/database.py
@@ -19,7 +19,7 @@ db_hostname = os.getenv("DB_HOSTNAME")
 db_port = os.getenv("DB_PORT")
 db_name = os.getenv("DB_NAME")
 
-db_password = parse.quote(db_password)
+db_password = parse.quote(db_password.encode('utf-8'))
 
 Sql_URL = f"mysql+asyncmy://{db_username}:{db_password}@{db_hostname}:{db_port}/{db_name}?charset=utf8mb4"
 
