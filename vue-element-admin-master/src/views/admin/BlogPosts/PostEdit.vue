1c4a1137330e8e6b4440bc3089197cc3cd4452e2
Exp1oit Yao <eitsxiaozhai@gmail.com>
2023-08-25 22:19:19 +0800

    修改markdown-添加markdown页面编辑

--- vue-element-admin-master/src/views/admin/BlogPosts/PostEdit.vue
+++ vue-element-admin-master/src/views/admin/BlogPosts/PostEdit.vue
@@ -1,26 +1,51 @@
 <template>
-  <div>
-    <template v-if="post.BlogId">
-      <p>文章ID: {{ post.BlogId }}</p>
-      <el-input v-if="editMode" v-model="post.author" />
-      <p v-else>作者: {{ post.author }}</p>
-      <el-input v-if="editMode" v-model="post.content" type="textarea" :rows="5" />
-      <p>创建时间: {{ post.created_at }}</p>
-      <markdown-editor v-model="post.content" />
-      <h1>博客介绍页面</h1>
-      <el-image :src="post.BlogIntroductionPicture" fit="contain" style="max-height: 300px" />
-    </template>
-    <template v-else>
-      Loading...
-    </template>
-    <template v-if="editMode">
-      <el-button type="success" @click="savePost">Save</el-button>
-      <el-button type="warning" @click="cancelEdit">Cancel</el-button>
-    </template>
-    <template v-else>
-      <el-button type="primary" @click="enterEditMode">Edit</el-button>
-    </template>
-  </div>
+  <el-container>
+    <el-main>
+      <div>
+        <template v-if="post.BlogId">
+          <el-input
+            v-model="post.title"
+            type="textarea"
+            autosize
+            placeholder="请输入内容"
+          />
+          <el-row :gutter="10" style="margin-top: 20px">
+            <el-col :xs="8" :sm="6" :md="4" :lg="3" :xl="1">
+              <div class="grid-content bg-purple" />
+            </el-col>
+            <el-col :xs="4" :sm="6" :md="8" :lg="9" :xl="11">
+              <div class="grid-content bg-purple-light">作者: {{ post.author }}</div>
+            </el-col>
+            <el-col :xs="4" :sm="6" :md="8" :lg="9" :xl="11">
+              <div class="grid-content bg-purple">创建时间
+                <el-date-picker
+                  v-model="value1"
+                  type="datetime"
+                  placeholder="选择日期时间"
+                  style="margin-bottom: 20px"
+                />
+              </div>
+            </el-col>
+            <el-col :xs="8" :sm="6" :md="4" :lg="3" :xl="1">
+              <div class="grid-content bg-purple-light" />
+            </el-col>
+          </el-row>
+          <markdown-editor v-model="post.content" />
+          <el-container style="margin-top: 20px;margin-bottom: 20px"><el-avatar shape="square" :size="350" :fit="fit" :src="post.BlogIntroductionPicture" /></el-container>
+        </template>
+        <template v-else>
+          Loading...
+        </template>
+        <template v-if="editMode">
+          <el-button type="success" @click="savePost">Save</el-button>
+          <el-button type="warning" @click="cancelEdit">Cancel</el-button>
+        </template>
+        <template v-else>
+          <el-button type="primary" @click="enterEditMode">Edit</el-button>
+        </template>
+      </div>
+    </el-main>
+  </el-container>
 </template>
 
 <script>
@@ -35,7 +60,9 @@ export default {
   data() {
     return {
       post: {},
-      editMode: false
+      editMode: false,
+      value1: '',
+      title: ''
     }
   },
   created() {
@@ -46,6 +73,8 @@ export default {
       try {
         const response = await BlogDetails(blog_id)
         this.post = response.data[0]
+        // 将 created_at 转换为 JavaScript Date 对象并设置为 value1
+        this.value1 = new Date(this.post.created_at)
       } catch (error) {
         console.error('API error:', error)
       }
