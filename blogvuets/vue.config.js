355e84d9dd6e1755c911eee837103be757081c69
Exp1oit Yao <eitsxiaozhai@gmail.com>
2023-09-21 18:51:21 +0800

    环境增加-添加API

--- blogvuets/vue.config.js
+++ blogvuets/vue.config.js
@@ -1,4 +1,10 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+module.exports = {
+  chainWebpack: (config) => {
+    config.plugin('define').tap((args) => {
+      const env = args[0];
+      // 这里使用了 process.env.VUE_APP_API_URL，确保加载对应环境的 API 地址
+      env['process.env']['VUE_APP_API_URL'] = JSON.stringify(process.env.VUE_APP_API_URL);
+      return args;
+    });
+  },
+};
