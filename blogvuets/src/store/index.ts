d51b33e3278ff284b9aecad6d3cb0a4d511aa272
Exp1oit Yao <eitsxiaozhai@gmail.com>
2023-09-22 23:10:29 +0800

    登录弹窗

--- blogvuets/src/store/index.ts
+++ blogvuets/src/store/index.ts
@@ -2,13 +2,22 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
-  },
-  getters: {
+    token: '', // 初始状态为空令牌
   },
   mutations: {
+    setToken(state, token) {
+      state.token = token;
+    },
+  },
+  getters: {
+    getToken(state) {
+      return state.token;
+    },
   },
   actions: {
+    // 其他操作...
   },
   modules: {
+    // 其他模块...
   }
 })
--- blogvuets/src/views/user/Userlogin.vue
+++ blogvuets/src/views/user/Userlogin.vue
@@ -1,41 +1,78 @@
 <script setup>
-import { ref } from 'vue';
-import axios from 'axios';
+import { ref, onMounted, onBeforeMount } from 'vue';
 import backApi from "@/Api/backApi";
-
+import { useRouter } from 'vue-router'; // 导入useRouter函数
+import { ElNotification } from 'element-plus';
 const loginForm = ref({
   username: '',
   password: '',
 });
 
+let router; // 声明路由器变量
+
+onBeforeMount(() => {
+  router = useRouter(); // 在组件挂载之前获取路由器实例
+});
+
+
+const token = ref(''); // 创建一个ref变量来存储令牌
+
+onMounted(() => {
+  // 在组件加载后从localStorage中加载令牌
+  const storedToken = localStorage.getItem("token");
+  if (storedToken) {
+    token.value = storedToken;
+  }
+});
+
 const login = async () => {
   try {
-    console.log(loginForm)
+    console.log(loginForm);
+    console.log(token)
     const response = await backApi.post('/generaluser/login', {
       username: loginForm.value.username,
       password: loginForm.value.password,
     });
-    // 根据后端返回的数据处理登录成功或失败的情况
+
     if (response.data.success) {
-      // 登录成功，可以进行页面跳转或其他操作
-      console.log('登录成功:', response.data.message);
+      const newToken = response.data.token;
+      localStorage.setItem("token", newToken);
+      token.value = newToken;
+
+
+      router.push('/blog'); // 使用router进行页面重定向
+      ElNotification({
+    title: 'Success',
+    message: '登录成功',
+    type: 'success',
+  })
     } else {
-      // 登录失败，处理错误消息
+        ElNotification({
+    title: 'Warning',
+    message: '登录失败',
+    type: 'warning',
+  })
       console.error('登录失败:', response.data.message);
     }
   } catch (error) {
+      ElNotification({
+    title: 'Warning',
+    message: '登录失败',
+    type: 'warning',
+  })
     console.error('登录失败:', error);
   }
 };
 </script>
 
 <template>
-  <el-form  :model="loginForm" label-width="80px" class="login-form">
+  <el-form :model="loginForm" label-width="80px" class="login-form">
     <el-form-item label="用户名" prop="username">
-      <el-input  @input="loginForm.username = $event" v-model="loginForm.username" placeholder="请输入用户名"></el-input>
+      <el-input @input="loginForm.username = $event" v-model="loginForm.username" placeholder="请输入用户名"></el-input>
     </el-form-item>
     <el-form-item label="密码" prop="password">
-      <el-input @input="loginForm.password = $event" type="password" v-model="loginForm.password" placeholder="请输入密码"></el-input>
+      <el-input @input="loginForm.password = $event" type="password" v-model="loginForm.password"
+                placeholder="请输入密码"></el-input>
     </el-form-item>
     <el-form-item>
       <el-button type="primary" @click="login">登录</el-button>
