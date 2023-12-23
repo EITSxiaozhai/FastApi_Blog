d80e23f409022ba11006cb3e35c64fb42178bfa1
EITSxiaozhai <watch.dog@qq.com>
2023-12-23 21:20:27 +0800

    添加pydantic对于数据库
    Signed-off-by: EITSxiaozhai <watch.dog@qq.com>

--- app/Fast_blog/schemas/schemas.py
+++ app/Fast_blog/schemas/schemas.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from pydantic import BaseModel, EmailStr
 from typing import Optional
 import sys
@@ -30,3 +32,81 @@ class SchemasUser(BaseModel):
     gender: bool
     UserUuid: str
     UserEmail: EmailStr
+
+
+
+from typing import Optional, List
+from pydantic import BaseModel, EmailStr, constr
+
+class UserPrivilegesModel(BaseModel):
+    NameId: int
+    privilegeName: str
+
+class UserModel(BaseModel):
+    UserId: int
+    username: str
+    userpassword: constr(min_length=8)  # 设置最小密码长度
+    gender: str
+    creation_time: datetime
+    Last_Login_Time: datetime
+    UserUuid: str
+    UserEmail: EmailStr
+    comments: List[Optional["CommentModel"]] = []
+
+class CommentModel(BaseModel):
+    id: int
+    parentId: Optional[int]
+
+    uid: int
+    blog_id: int
+    address: str
+    content: str
+    likes: int
+    createTime: str
+    contentImg: str
+    user: Optional[UserModel]
+    replies: List[Optional["CommentModel"]] = []
+
+class AdminUserModel(BaseModel):
+    UserId: int
+    username: str
+    userpassword: constr(min_length=8)
+    gender: str
+    creation_time: datetime
+    Last_Login_Time: datetime
+    UserUuid: str
+    UserEmail: EmailStr
+    userPrivileges: Optional[int]
+    privileges: Optional[UserPrivilegesModel]
+
+class BlogModel(BaseModel):
+    BlogId: int
+    title: str
+    content: bytes
+    BlogIntroductionPicture: str
+    created_at: datetime
+    NumberLikes: int
+    NumberViews: int
+    author: str
+    admin_id: int
+    ratings: List[Optional["BlogRatingModel"]] = []
+    comments: List[Optional[CommentModel]] = []
+
+class PowerMetersModel(BaseModel):
+    PowerId: int
+    DataNum: datetime
+    electricityNum: str
+    PowerConsumption: str
+    AveragePower: str
+
+class BlogRatingModel(BaseModel):
+    id: int
+    blog_id: int
+    rating: float
+    blog: Optional[BlogModel]
+
+class VoteModel(BaseModel):
+    id: int
+    device_id: str
+    blog_id: str
+    vote_count: int
