649552bc50f1e644eda1eb8dc2389e48ff5fcace
EITSxiaozhai <watch.dog@qq.com>
2023-12-28 20:32:51 +0800

    优化token存储
    Signed-off-by: EITSxiaozhai <watch.dog@qq.com>

--- blogvuets/src/store/index.ts
+++ blogvuets/src/store/index.ts
@@ -1,17 +1,31 @@
-import { createStore } from 'vuex'
+import { createStore } from 'vuex';
 
 export default createStore({
   state: {
     token: '', // 初始状态为空令牌
+    username: '', // 添加一个用于用户名的新属性
   },
   mutations: {
+    setTokenAndUsername(state, { token, username }) {
+    state.token = token;
+    state.username = username;
+    },
     setToken(state, token) {
       state.token = token;
+    },
+    setUsername(state, username) {
+      state.username = username;
     },
   },
   getters: {
     getToken(state) {
+
       return state.token;
+    },
+    getUsername(state) {
+      // Example getter
+
+      return state.username
     },
   },
   actions: {
@@ -20,4 +34,4 @@ export default createStore({
   modules: {
     // 其他模块...
   }
-})
+});
