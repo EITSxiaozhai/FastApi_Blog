ab2aa7fc2b6e51d4fafacad2f18413be557136ff
EITSxiaozhai <watch.dog@qq.com>
2024-01-18 16:00:13 +0800

    添加雪花背景飘落
    Signed-off-by: EITSxiaozhai <watch.dog@qq.com>

--- blogvuets/src/App.vue
+++ blogvuets/src/App.vue
@@ -19,11 +19,43 @@ useHead({
   </head>
   <div id="app">
     <router-view/>
+    <div v-for="item of 500" :key="item" class="snowflake"></div>
   </div>
 </template>
 
 
-<style>
+<style lang="scss">
+.snowflake {
+  --size: 5vw;
+  width: var(--size);
+  height: var(--size);
+  background: url("../static/img/xuehua.png") no-repeat;
+  background-size: 100% 100%;
+  position: fixed;
+  top: -5vh;
+  z-index: -1;
+}
+
+@keyframes snowfall {
+  100% {
+    transform: translate3d(var(--end), 100vh, 0);
+  }
+}
+
+@for $i from 0 through 500 {
+  .snowflake:nth-child(#{$i}) {
+    //每个雪花的大小
+    --size: #{random(10) * 0.1}vw;
+    //雪花移动目标点 -70后是负数 这样雪花会向左下方飘落
+    --end: #{random(20) - 70}vw;
+    left: #{random(150)}vw;
+    animation: snowfall #{5 + random(8)}s linear infinite;
+    animation-delay: -#{random(10)}s;
+  }
+}
+
+
+
 html, body {
   height: 100%;
   width: 100%;
