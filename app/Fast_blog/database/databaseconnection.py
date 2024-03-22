c101507344432e07eff1b09a16a9897cbcea6f33
EITSxiaozhai <eitsxiaozhai@gmail.com>
2024-03-22 14:02:11 +0800

    删除多余注释
    Signed-off-by: EITSxiaozhai <eitsxiaozhai@gmail.com>

--- app/Fast_blog/database/databaseconnection.py
+++ app/Fast_blog/database/databaseconnection.py
@@ -21,14 +21,12 @@ db_password = parse.quote(db_password.encode('utf-8'))
 
 Sql_URL = f"mysql+asyncmy://{db_username}:{db_password}@{db_hostname}:{db_port}/{db_name}?charset=utf8mb4"
 
-##创建异步连接
+#创建异步连接
 engine = create_async_engine(Sql_URL)
 
-##创建ORM模型操作方法
+#创建ORM模型操作方法
 Base = declarative_base()
 
-##实例化异步操作
+#实例化异步操作
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False, class_=AsyncSession)
 db_session = async_scoped_session(SessionLocal, scopefunc=current_task)
-
-##创建metadata操作对象
