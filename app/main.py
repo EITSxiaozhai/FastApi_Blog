80b70deda4a951db00e45b1f7095bdc62401adc4
EITSxiaozhai <watch.dog@qq.com>
2024-06-22 18:09:19 +0800

    修改跨域IP地址 Signed-off-by: EITSxiaozhai <eitsxiaozhai@gmail.com>
    Signed-off-by: EITSxiaozhai <watch.dog@qq.com>

--- app/main.py
+++ app/main.py
@@ -35,7 +35,7 @@ app.add_middleware(
     CORSMiddleware,
     ##此处URL用来允许通过的域名。提高安全性，需要根据你的进行修改
     allow_origins=["https://blog.exploit-db.xyz","https://blogapi.exploit-db.xyz", "https://blogapi.exploitblog.eu.org", 'http://192.168.0.149:9527',
-                   'http://127.0.0.1:8000','http://192.168.0.149:5173', 'http://192.168.0.13:9527', 'http://192.168.0.13:5173','https://zpwl002.oss-cn-hangzhou.aliyuncs.com',
+                   'http://127.0.0.1:8000','http://192.168.0.149:5173', 'http://192.168.0.149:5174','http://192.168.0.13:9527', 'http://192.168.0.13:5173','https://zpwl002.oss-cn-hangzhou.aliyuncs.com',
                    "https://static.cloudflareinsights.com"],
     allow_credentials=False,
     allow_methods=["*"],
