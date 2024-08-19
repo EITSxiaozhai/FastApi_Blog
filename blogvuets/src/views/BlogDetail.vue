2b237e11a821ae40c73d6086605c0da631f64b0a
exploit <eitsxiaozhai@gmail.com>
2024-08-19 15:28:28 +0800

    栅栏布局修改
    Signed-off-by: exploit <eitsxiaozhai@gmail.com>

--- blogvuets/src/views/BlogDetail.vue
+++ blogvuets/src/views/BlogDetail.vue
@@ -481,6 +481,8 @@ const redirectToUserProfile = () => {
 
 
   <el-container class="affix-container">
+     <el-row>
+      <el-col :xs="11" :sm="3" :md="4" :lg="6" :xl="10" class="hidden-md-and-down">
         <el-aside>
           <el-affix target=".affix-container" :offset="270">
             <el-card style="height: 30vh">
@@ -510,7 +512,10 @@ const redirectToUserProfile = () => {
             </el-card>
           </el-affix>
         </el-aside>
+         </el-col>
 
+
+      <el-col :xs="24" :sm="24" :md="19" :lg="17" :xl="13">
         <el-main>
           <el-card style="margin-top: 20px;padding-bottom: 10%" v-if="!isLoading">
             <div v-for="(item, index) in data.data" :key="index" class="text item">
@@ -531,6 +536,9 @@ const redirectToUserProfile = () => {
             </el-card>
           </div>
         </el-main>
+
+      </el-col>
+    </el-row>
   </el-container>
   <el-backtop/>
 </template>
