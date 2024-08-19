88ed767028fc0dddfc49cc98ae32a89adf937a32
exploit <eitsxiaozhai@gmail.com>
2024-08-19 13:36:17 +0800

    删除无用css
    Signed-off-by: exploit <eitsxiaozhai@gmail.com>

--- blogvuets/src/App.vue
+++ blogvuets/src/App.vue
@@ -25,45 +25,7 @@ useHead({
 
 
 <style lang="scss">
-//.snowflake {
-//  --size: 0vw;
-//  width: var(--size);
-//  height: var(--size);
-//  background: url("../static/img/xuehua.png") no-repeat;
-//  background-size: 100% 100%;
-//  position: fixed;
-//  top: -5vh;
-//  z-index: -1;
-//}
-//
-//@keyframes snowfall {
-//  100% {
-//    transform: translate3d(var(--end), 100vh, 0);
-//  }
-//}
-//
-//@for $i from 0 through 500 {
-//  .snowflake:nth-child(#{$i}) {
-//    //每个雪花的大小
-//    --size: #{random(10) * 0.1}vw;
-//    //雪花移动目标点 -70后是负数 这样雪花会向左下方飘落
-//    --end: #{random(20) - 70}vw;
-//    left: #{random(150)}vw;
-//    animation: snowfall #{10 + random(10)}s linear infinite;
-//    animation-delay: -#{random(10)}s;
-//  }
-//}
 
-
-//html, body {
-//  height: 100%;
-//  width: 100%;
-//  margin: 0;
-//  padding: 0;
-//  background: rgb(233, 233, 235);
-//  font-family:-apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen-Sans, Ubuntu, Cantarell, Helvetica Neue, sans-serif;
-//}
-//
 #app, body {
   font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen-Sans, Ubuntu, Cantarell, Helvetica Neue, sans-serif;
   height: 100%; /* 使#app占满全屏高度 */
