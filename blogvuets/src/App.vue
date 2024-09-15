5d725fd4c8a935d3aa02d382dfa6a275ee598a1d
EITSxiaozhai <watch.dog@qq.com>
2024-09-15 17:31:23 +0800

    修改设置SEO方法
    Signed-off-by: EITSxiaozhai <watch.dog@qq.com>

--- blogvuets/src/App.vue
+++ blogvuets/src/App.vue
@@ -1,25 +1,22 @@
 <script setup>
 import {ref} from 'vue';
-import {useHead} from '@unhead/vue';
+// import {useHead} from '@unhead/vue';
+import { useMeta } from 'vue-meta';
 
-const myPage = ref({description: 'Exp1oit的博客。分享Linux服务器搭建运维,学习Python技术,vue3,利用Fastapi,Django,mysql的技术搭建个人网站。记录一下自己的学习成果'})
-const title = ref('title')
-const metakeywords = ref('Linux,Python,Vue,运维,云服务,Seo,计算机教学,计算机基础')
+const description = ref('Exp1oit的博客。分享Linux服务器搭建运维,学习Python技术,vue3,利用Fastapi,Django,mysql的技术搭建个人网站。记录一下自己的学习成果')
+const keywords = ref('Linux,Python,Vue,运维,云服务,Seo,K8s,Devops,运维开发')
 
 
-useHead({
-  meta: [{name: 'description', content: () => myPage.value.description},
-    {name: 'keywords', content: () => metakeywords}],
-})
+// 使用 `useMeta` 设置 meta 信息
+const { meta } = useMeta({
+  keywords: keywords.value,
+});
+
 </script>
 
 <template>
-  <head>
-    <meta name="keywords" content="">
-  </head>
-
+  <metainfo></metainfo>
   <div id="app">
-
     <router-view/>
 <!--    <div v-for="item of 500" :key="item" class="snowflake"></div>-->
   </div>
