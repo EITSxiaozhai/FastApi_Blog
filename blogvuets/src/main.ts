4b16db987168224dff6376c9a7a05d94824452bd
EITSxiaozhai <watch.dog@qq.com>
2024-09-15 17:29:35 +0800

    添加vue-meta包
    Signed-off-by: EITSxiaozhai <watch.dog@qq.com>

--- blogvuets/src/main.ts
+++ blogvuets/src/main.ts
@@ -6,14 +6,13 @@ import UndrawUi from 'undraw-ui'
 import 'undraw-ui/dist/style.css'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
-import {createHead} from '@unhead/vue'
 import { createMetaManager } from 'vue-meta'
 import "animate.css"
 import 'animate.css/animate.compat.css'
 import 'element-plus/theme-chalk/display.css'
 
 
-const head = createHead()
+
 createApp(App).use(store).use(router).use(createMetaManager(false, {
     meta: { tag: 'meta', nameless: true }
-})).use(UndrawUi).use(ElementPlus).use(head).mount('#app')
+})).use(UndrawUi).use(ElementPlus).mount('#app')
