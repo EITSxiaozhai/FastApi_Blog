bdc50f59dbd7a91fb6d603ffb595a4cfa8078c9b
EITSxiaozhai <watch.dog@qq.com>
2024-09-18 21:56:46 +0800

    移动接口
    Signed-off-by: EITSxiaozhai <watch.dog@qq.com>

--- blogvuets/src/api/Blog/blogapig.ts
+++ blogvuets/src/api/Blog/blogapig.ts
@@ -28,6 +28,30 @@ export function postCommentList(blogId: string) {
   });
 }
 
+
+export function Postlist(blogId: string) {
+  return backApi({
+    url: `/views/blog/BlogIndex`,
+    method: 'get'
+  });
+}
+
+
+export function fetchBlogIndex({ page, pageSize }: { page: number; pageSize: number }) {
+  return backApi({
+    url: `/views/blog/BlogIndex?initialLoad=false&page=${page}&pageSize=${pageSize}`,
+    method: 'get'
+  });
+}
+
+export function GoogleUVPV(blogId: string) {
+  return backApi({
+    url: `/views/blogs/total_uvpv`,
+    method: 'get'
+  });
+}
+
+
 export function postCommentSave(blogId: string, str: string, token: string) {
   return backApi({
     url: `/generaluser/commentsave/vueblogid=${blogId}`,
@@ -40,3 +64,4 @@ export function postCommentSave(blogId: string, str: string, token: string) {
     }
   });
 }
+
--- blogvuets/src/api/User/userapi.ts
+++ blogvuets/src/api/User/userapi.ts
@@ -1,2 +1,43 @@
 import backApi  from "@/utils/backApi";
 
+export function UserLogin({data}: { data: any }) {
+  return backApi({
+    url: '/generaluser/login',
+    method: 'post',
+    data
+  })
+}
+
+
+export function RegUser({data}: { data: any }) {
+  return backApi({
+    url: '/generaluser/reguser',
+    method: 'post',
+    data
+  })
+}
+
+
+export function CheckUserName({data}: { data: any }) {
+  return backApi({
+    url: '/generaluser/check-username',
+    method: 'post',
+    data
+  })
+}
+
+export function SentMailCod({data}: { data: any }) {
+  return backApi({
+    url: '/generaluser/emailcod',
+    method: 'post',
+    data
+  })
+}
+
+export function UploadUserAvatar({data}: { data: any }) {
+  return backApi({
+    url: '/generaluser/reg',
+    method: 'post',
+    data
+  })
+}
