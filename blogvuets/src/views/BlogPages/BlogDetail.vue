3ebe2a834b014a43b9d7b63d24bc485af932eee0
xuyao <xuyao@dreame.tech>
2024-11-04 14:44:35 +0800

    优化前端标题以及详情显示方法
    Signed-off-by: xuyao <xuyao@dreame.tech>

--- blogvuets/src/views/BlogPages/BlogDetail.vue
+++ blogvuets/src/views/BlogPages/BlogDetail.vue
@@ -550,7 +550,7 @@ const redirectToUserProfile = () => {
   </el-container>
   <div>
     <el-card
-        style="margin: 3% auto; width: 100%; box-shadow: 0 2px 12px 0 rgba(0,0,0,0.1);"
+        style="margin: 3% auto; width: 99%; box-shadow: 0 2px 12px 0 rgba(0,0,0,0.1);"
         v-if="!isLoading"
     >
       <div v-for="(item, index) in data.data" :key="index" class="text-item">
