c2c025cfebf07ad14d1317becf5226c1e5326507
xuyao <xuyao@dreame.tech>
2025-02-14 11:43:41 +0800

    Revert "移动数据库迁移目录"
    This reverts commit bd9c71a93e5f9a9253deb84f5d38ca562f358271.

--- alembic/env.py
+++ alembic/env.py
@@ -25,7 +25,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-from Fast_blog.model.models import Base
+from app.Fast_blog.model.models import Base
 target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
