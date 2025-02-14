34e3281f26c5c94f5fded9754adb3c1ce0d22be2
xuyao <xuyao@dreame.tech>
2025-02-14 17:06:38 +0800

    修改启动脚本
    Signed-off-by: xuyao <xuyao@dreame.tech>

--- app/start.sh
+++ app/start.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-set -e  # 如果任何命令失败，则退出脚本
-echo "SQL_URL is set to ${SQL_URL}"
-# 执行数据库迁移
+# Let the DB start
+sleep 10;
+# Run migrations
 alembic upgrade head
-
 # 启动 Uvicorn 服务
 exec uvicorn main:app --host 0.0.0.0 --port 80 --proxy-headers --forwarded-allow-ips "172.17.0.1"
