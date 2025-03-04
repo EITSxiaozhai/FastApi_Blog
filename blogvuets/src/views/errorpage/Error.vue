c32c59f0601e309ca0d5864c04b853282568e43f
xuyao <xuyao@dreame.tech>
2025-03-04 16:33:29 +0800

    添加404页面
    Signed-off-by: xuyao <xuyao@dreame.tech>

---
+++ blogvuets/src/views/errorpage/Error.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="not-found">
+    <img src="public/404.gif" alt="" />
+  </div>
+</template>
+
+<style scoped>
+.not-found {
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+}
+.not-found img {
+  width: 100%;
+  height: 100%;
+}
+</style>
