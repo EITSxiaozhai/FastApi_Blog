b5875e47c22c088574b156a615b9d7129a297ce8
xuyao <xuyao@dreame.tech>
2025-03-27 15:45:01 +0800

    增加暗色模式，手动切换按钮
    Signed-off-by: xuyao <xuyao@dreame.tech>

--- blogvuets/src/main.ts
+++ blogvuets/src/main.ts
@@ -10,6 +10,7 @@ import {createHead} from '@unhead/vue'
 import "animate.css"
 import 'animate.css/animate.compat.css'
 import 'element-plus/theme-chalk/display.css'
+import 'element-plus/theme-chalk/dark/css-vars.css'
 
 const head = createHead()
 createApp(App).use(store).use(router).use(UndrawUi).use(ElementPlus).use(head).mount('#app')
