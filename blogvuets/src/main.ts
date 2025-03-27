35f4a163fcdc698f970d8bb65fbda96eda31c8fa
xuyao <xuyao@dreame.tech>
2025-03-27 16:24:34 +0800

    添加暗色css
    Signed-off-by: xuyao <xuyao@dreame.tech>

--- blogvuets/src/main.ts
+++ blogvuets/src/main.ts
@@ -11,6 +11,7 @@ import "animate.css"
 import 'animate.css/animate.compat.css'
 import 'element-plus/theme-chalk/display.css'
 import 'element-plus/theme-chalk/dark/css-vars.css'
+import './assets/css/css-vars.css'
 
 const head = createHead()
 createApp(App).use(store).use(router).use(UndrawUi).use(ElementPlus).use(head).mount('#app')
