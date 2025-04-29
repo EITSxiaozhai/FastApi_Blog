ae8507f53167681cbbc7fb6363eb222368f1d3a4
xuyao <xuyao@dreame.tech>
2025-04-29 16:39:50 +0800

    传递真实ip

--- app/start.sh
+++ app/start.sh
@@ -4,4 +4,4 @@ sleep 10;
 # Run migrations
 alembic upgrade head
 # 启动 Uvicorn 服务
-exec uvicorn main:app --host 0.0.0.0 --port 80 --proxy-headers --forwarded-allow-ips "172.17.0.1"
+exec uvicorn main:app --host 0.0.0.0 --port 80 --proxy-headers --forwarded-allow-ips "*"
