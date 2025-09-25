183066bc9717c36ada52f66908c1a12b6228165f
EITSxiaozhai <eitsxiaozhai@gmail.com>
2025-09-25 16:05:21 +0800

    修改数据库模型-以及模型版本-建议直接重新迁移

--- app/alembic/versions/4f349205aa1a_add_description.py
+++ app/alembic/versions/4f349205aa1a_add_description.py
@@ -13,7 +13,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision: str = '4f349205aa1a'
-down_revision: Union[str, None] = None
+down_revision: Union[str, None] = '5a76165bab51'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
