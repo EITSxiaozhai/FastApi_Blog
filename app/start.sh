01d191d22bd147254cde21a5554cfad2568aef80
EITSxiaozhai <eitsxiaozhai@gmail.com>
2026-04-15 14:11:35 +0800

    设置启动日志等级

--- app/start.sh
+++ app/start.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-# Let the DB start
-sleep 10;
 # Run migrations
 alembic upgrade head
 # 启动 Uvicorn 服务
-exec uvicorn main:app --host 0.0.0.0 --port 80 --proxy-headers --forwarded-allow-ips "*"
+exec uvicorn main:app --host 0.0.0.0 --port 80 --proxy-headers --forwarded-allow-ips "*" --no-access-log
